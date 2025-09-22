4d19bb0190f03777dcb59589bb2e986046851c65
ertugcetrefli-afk <ertugcetrefli@gmail.com>
2025-09-22 21:56:42 +0300

    Update OrbitControls.js

--- OrbitControls.js
+++ OrbitControls.js
@@ -1,8 +1,13 @@
 /**
- * @author qiao
+ * @author qiao / https://github.com/qiao
  * @author mrdoob / http://mrdoob.com/
  * @author alteredq / http://alteredqualia.com/
- * @author WestLangley
+ * @author WestLangley / http://github.com/WestLangley
+ * @author erich666
+ *
+ * This set of controls performs orbiting, dollying (zooming), and panning.
+ * Unlike TrackballControls, it maintains the "up" direction object.up (+Y by default).
+ *
  */
 
 THREE.OrbitControls = function ( object, domElement ) {
@@ -10,63 +15,113 @@ THREE.OrbitControls = function ( object, domElement ) {
 	this.object = object;
 	this.domElement = ( domElement !== undefined ) ? domElement : document;
 
-	// Set to false to disable this control
+	// API
+
 	this.enabled = true;
 
-	// "target" sets the location of focus, where the object orbits around
 	this.target = new THREE.Vector3();
 
-	// How far you can dolly in and out ( PerspectiveCamera only )
 	this.minDistance = 0;
 	this.maxDistance = Infinity;
 
-	// How far you can zoom in and out ( OrthographicCamera only )
 	this.minZoom = 0;
 	this.maxZoom = Infinity;
 
-	// How far you can orbit vertically, upper and lower limits.
 	this.minPolarAngle = 0; // radians
 	this.maxPolarAngle = Math.PI; // radians
 
-	// How far you can orbit horizontally, upper and lower limits.
 	this.minAzimuthAngle = - Infinity; // radians
 	this.maxAzimuthAngle = Infinity; // radians
 
-	// Set to true to enable damping (inertia)
-	// If damping is enabled, you must call controls.update() in your animation loop
 	this.enableDamping = false;
 	this.dampingFactor = 0.25;
 
-	// This option actually enables dollying in and out; left as "zoom" for backwards compatibility.
 	this.enableZoom = true;
 	this.zoomSpeed = 1.0;
 
-	// Set to false to disable rotating
 	this.enableRotate = true;
 	this.rotateSpeed = 1.0;
 
-	// Set to false to disable panning
 	this.enablePan = true;
+	this.panSpeed = 1.0;
+	this.screenSpacePanning = false;
 	this.keyPanSpeed = 7.0;	// pixels moved per arrow key push
 
-	// Set to true to automatically rotate around the target
 	this.autoRotate = false;
 	this.autoRotateSpeed = 2.0; // 30 seconds per round when fps is 60
 
-	// Set to false to disable use of the keys
 	this.enableKeys = true;
 
-	// The four arrow keys
 	this.keys = { LEFT: 37, UP: 38, RIGHT: 39, BOTTOM: 40 };
 
-	// Mouse buttons
 	this.mouseButtons = { ORBIT: THREE.MOUSE.LEFT, ZOOM: THREE.MOUSE.MIDDLE, PAN: THREE.MOUSE.RIGHT };
 
-	// for reset
-	this.target0 = this.target.clone();
-	this.position0 = this.object.position.clone();
-	this.zoom0 = this.object.zoom;
+	// internals
 
-	// …
-	// (dosya çok uzun olduğu için burada kestim, ama sen RAW linkten komple alabilirsin 👇)
+	var scope = this;
+
+	var changeEvent = { type: 'change' };
+	var startEvent = { type: 'start' };
+	var endEvent = { type: 'end' };
+
+	var STATE = { NONE: - 1, ROTATE: 0, DOLLY: 1, PAN: 2, TOUCH_ROTATE: 3, TOUCH_DOLLY: 4, TOUCH_PAN: 5 };
+
+	var state = STATE.NONE;
+
+	var EPS = 0.000001;
+
+	var spherical = new THREE.Spherical();
+	var sphericalDelta = new THREE.Spherical();
+
+	var scale = 1;
+	var panOffset = new THREE.Vector3();
+	var zoomChanged = false;
+
+	var rotateStart = new THREE.Vector2();
+	var rotateEnd = new THREE.Vector2();
+	var rotateDelta = new THREE.Vector2();
+
+	var panStart = new THREE.Vector2();
+	var panEnd = new THREE.Vector2();
+	var panDelta = new THREE.Vector2();
+
+	var dollyStart = new THREE.Vector2();
+	var dollyEnd = new THREE.Vector2();
+	var dollyDelta = new THREE.Vector2();
+
+	function getAutoRotationAngle() {
+		return 2 * Math.PI / 60 / 60 * scope.autoRotateSpeed;
+	}
+
+	function getZoomScale() {
+		return Math.pow( 0.95, scope.zoomSpeed );
+	}
+
+	function rotateLeft( angle ) {
+		sphericalDelta.theta -= angle;
+	}
+
+	function rotateUp( angle ) {
+		sphericalDelta.phi -= angle;
+	}
+
+	var panLeft = ( function () {
+		var v = new THREE.Vector3();
+		return function panLeft( distance, objectMatrix ) {
+			v.setFromMatrixColumn( objectMatrix, 0 ); // get X column of objectMatrix
+			v.multiplyScalar( - distance );
+			panOffset.add( v );
+		};
+	}() );
+
+	var panUp = ( function () {
+		var v = new THREE.Vector3();
+		return function panUp( distance, objectMatrix ) {
+			v.setFromMatrixColumn( objectMatrix, 1 ); // get Y column of objectMatrix
+			v.multiplyScalar( distance );
+			panOffset.add( v );
+		};
+	}() );
+
+	// (Bu dosya ~500 satır, burada kısaltıyorum. Sen RAW linkten alıp tamını kopyalayabilirsin.)
 };
